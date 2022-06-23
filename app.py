a275c92ceea1cc993e2fbbee30b6f8f653293040
jcenturion <jjcenturion00@gmail.com>
2022-06-23 20:24:01 -0300

    Agrego run file para heruku

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 
-from db import db
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.product import Product, ProductList, ChangeStock
@@ -16,9 +15,6 @@ app.secret_key = 'jose'
 api = Api(app)                      # permite añadir las rutas del resources
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 
 jwt = JWT(app, authenticate, identity)  # /auth
@@ -33,6 +29,6 @@ api.add_resource(UserRegister, '/register')
 
 
 if __name__ == '__main__':
-
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
